52e277829c8e410177aedd195727f5fe5eb0a8ac
kalyan-git-repo <kalyancreamcollar@gmail.com>
2025-10-17 16:22:29 +0530

    chatcontained & messageinput

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app = express()
 const PORT = ENV.PORT || 3000
 const _dirname = path.resolve()
 
-app.use(express.json());
+app.use(express.json({ limit : "5mb"}));
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }))
 app.use(cookieParser())
 
